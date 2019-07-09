780d7e588477f963ca225953f4101d6e9b305894
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-07-08 20:12:37 -0700

    Updated error message

--- config/orm.js
+++ config/orm.js
@@ -1,99 +1,103 @@
 var connection = require("./connection.js");
 
 function printQuestionMarks(num) {
-    var arr = [];
-  
-    for (var i = 0; i < num; i++) {
-      arr.push("?");
-    }
-  
-    return arr.toString();
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
   }
 
-  function objToSql(ob) {
-    var arr = [];
-  
-    // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-      var value = ob[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        // e.g. {sleepy: true} => ["sleepy=true"]
-        arr.push(key + "=" + value);
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
       }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
     }
-  
-    // translate array of strings to a single comma-separated string
-    return arr.toString();
   }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
 
 var orm = {
-    selectAll: function(tableInput, cb) {
-        var queryString = "SELECT * FROM ??";
-        connection.query(queryString, tableInput, function(err, result) {
-            if (err) throw err;
-            console.log("Call back of the result");
-            cb(result);
-            console.log("End of callback");
-        });
-    }, 
-    // How generic are we supposed to make this orm?
-    // insertOne: function(tableInput, newName, devoured) {
-    //     var queryString = "INSERT INTO ? SET ?";
-    //     connection.query(queryString, [tableInput, {
-    //         burger_name: newName, 
-    //         devoured: devoured
-    //     }], function(err, result) {
-    //         if (err) throw err;
-    //         console.log(result);
-    //     });
-    // }, 
-    insertOne: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
+  selectAll: function (tableInput, cb) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, tableInput, function (err, result) {
+      if (err) {
+        console.log("Error");
+        console.log(err);
+        throw err;
+      };
+      console.log("Call back of the result");
+      cb(result);
+      console.log("End of callback");
+    });
+  },
+  // How generic are we supposed to make this orm?
+  // insertOne: function(tableInput, newName, devoured) {
+  //     var queryString = "INSERT INTO ? SET ?";
+  //     connection.query(queryString, [tableInput, {
+  //         burger_name: newName, 
+  //         devoured: devoured
+  //     }], function(err, result) {
+  //         if (err) throw err;
+  //         console.log(result);
+  //     });
+  // }, 
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
 
-        console.log("Query string");
-        console.log(queryString);
+    console.log("Query string");
+    console.log(queryString);
 
-        console.log("Values");
-        console.log(vals);
+    console.log("Values");
+    console.log(vals);
 
-        connection.query(queryString, vals, function(err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    },
-    //will fill out the specifics of this method later
-    updateOne: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  //will fill out the specifics of this method later
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
 
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
 
-        console.log("Query String");
-        console.log(queryString);
-        connection.query(queryString, function(err, result) {
-            if (err) {
-                throw err;
-            }
+    console.log("Query String");
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
 
-            cb(result);
-        })
-    }
+      cb(result);
+    })
+  }
 };
 
 module.exports = orm;
